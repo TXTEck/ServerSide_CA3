523786a1d18b3700df07006ec2a4398623fa3211
TXTEck <91459117+TXTEck@users.noreply.github.com>
2024-05-10 03:00:35 +0800

    migration code updated

--- database/migrations/2024_05_09_183825_create_users_table.php
+++ database/migrations/2024_05_09_183825_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-Class Users extends Migration
+Class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
